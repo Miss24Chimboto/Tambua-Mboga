f095b04ed5ebfccb4dd551219006531f38f54e97
kendimuthomi <kendijackline42@gmail.com>
2023-02-21 13:53:51 +0000

    forms

--- flask_mboga/main.py
+++ flask_mboga/main.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '611e204927d4e611f723b1935726c350a1c05ade'
+
 
 @app.route("/")
 @app.route("/home")
@@ -18,6 +22,16 @@ def recipe():
 @app.route("/nutritional_value")
 def nutritional_valuesource():
     return render_template('nutrtional_value.html')
+
+@app.route("/register")
+def register():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
+
+@app.route("/login")
+def login():
+        form = LoginForm()
+        return render_template('login.html', form=form)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
