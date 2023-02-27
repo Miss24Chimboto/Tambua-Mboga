5d44447a0cad5c61bc7d69ab8bdfb8f06385e381
kendimuthomi <kendijackline42@gmail.com>
2023-02-27 13:27:14 +0000

    database cleanup

--- flask_mboga/__init__.py
+++ flask_mboga/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '611e204927d4e611f723b1935726c350a1c05ade'
+app.config['SECRET_KEY'] = ''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://tam_mysql_user:tam_mysql_pwd@localhost/tam_mysql_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
